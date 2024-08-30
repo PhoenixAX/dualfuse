d3f9288e83970c3f6ac8c901d69a9aaef9c04d8d
PhoenixAX <61862009+PhoenixAX@users.noreply.github.com>
2024-08-30 17:04:14 +0800

    Add files via upload

---
+++ network/spvcnn.py
@@ -0,0 +1,273 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: Xu Yan
+@file: spvcnn.py
+@time: 2021/12/16 22:41
+'''
+import torch
+import torch_scatter
+import spconv.pytorch as spconv
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+from network.basic_block import Lovasz_loss
+from network.base_model import LightningBaseModel
+from network.basic_block import SparseBasicBlock
+from network.voxel_fea_generator import voxel_3d_generator, voxelization
+from network.vfe import  SphereFormer
+
+
+class point_encoder(nn.Module):
+    def __init__(self, in_channels, out_channels, scale,layer_id):
+        super(point_encoder, self).__init__()
+        self.scale = scale
+        self.layer_id=layer_id
+        self.layer_in = nn.Sequential(
+            nn.Linear(in_channels, out_channels),
+            nn.LeakyReLU(0.1, True),
+        )
+        
+        self.PPmodel = nn.Sequential(
+            nn.Linear(in_channels, out_channels // 2),
+            nn.LeakyReLU(0.1, True),
+            nn.BatchNorm1d(out_channels // 2),
+            nn.Linear(out_channels // 2, out_channels // 2),
+            nn.LeakyReLU(0.1, True),
+            nn.BatchNorm1d(out_channels // 2),
+            nn.Linear(out_channels // 2, out_channels),
+            nn.LeakyReLU(0.1, True),
+        )
+        self.layer_out = nn.Sequential(
+            nn.Linear(2 * out_channels, out_channels),
+            nn.LeakyReLU(0.1, True),
+            nn.Linear(out_channels, out_channels))
+
+    @staticmethod
+    def downsample(coors, p_fea, scale=2):
+        batch = coors[:, 0:1]
+        coors = coors[:, 1:] // scale
+        inv = torch.unique(torch.cat([batch, coors], 1), return_inverse=True, dim=0)[1]
+        return torch_scatter.scatter_mean(p_fea, inv, dim=0), inv
+
+    def forward(self, features, data_dict):
+        output, inv = self.downsample(data_dict['coors'], features)
+        
+        identity=self.layer_in(features)
+        
+        output = self.PPmodel(output)[inv]
+        ###
+        #output=self.att(output, data_dict['ref_xyz'],data_dict['batch_idx'])
+        #output =self.reshape_(output)
+        ###
+        output = torch.cat([identity, output], dim=1)
+
+        v_feat = torch_scatter.scatter_mean(
+            self.layer_out(output[data_dict['coors_inv']]),
+            data_dict['scale_{}'.format(self.scale)]['coors_inv'],
+            dim=0
+        )
+        data_dict['coors'] = data_dict['scale_{}'.format(self.scale)]['coors']
+        data_dict['coors_inv'] = data_dict['scale_{}'.format(self.scale)]['coors_inv']
+        data_dict['full_coors'] = data_dict['scale_{}'.format(self.scale)]['full_coors']
+
+        return v_feat
+
+
+class SPVBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, indice_key, scale, last_scale, spatial_shape):
+        super(SPVBlock, self).__init__()
+        self.scale = scale
+        self.indice_key = indice_key
+        self.layer_id = indice_key.split('_')[1]
+        self.last_scale = last_scale
+        self.spatial_shape = spatial_shape
+        self.v_enc = spconv.SparseSequential(
+            SparseBasicBlock(in_channels, out_channels, self.indice_key),
+            SparseBasicBlock(out_channels, out_channels, self.indice_key),
+        )
+        self.p_enc = point_encoder(in_channels, out_channels, scale,self.layer_id )
+       
+        
+            
+
+
+    def forward(self, data_dict):
+        coors_inv_last = data_dict['scale_{}'.format(self.last_scale)]['coors_inv']
+        coors_inv = data_dict['scale_{}'.format(self.scale)]['coors_inv']
+
+        # voxel encoder
+        v_fea = self.v_enc(data_dict['sparse_tensor'])
+        
+        data_dict['layer_{}'.format(self.layer_id)] = {}
+        data_dict['layer_{}'.format(self.layer_id)]['pts_feat'] = v_fea.features
+        data_dict['layer_{}'.format(self.layer_id)]['full_coors'] = data_dict['full_coors']
+        v_fea_inv = torch_scatter.scatter_mean(v_fea.features[coors_inv_last], coors_inv, dim=0)
+        
+        # point encoder
+        p_fea = self.p_enc(
+            features=data_dict['sparse_tensor'].features+v_fea.features,
+            data_dict=data_dict
+        )
+       
+
+
+
+        #v_fea_inv=self.reshape_1(v_fea_inv)
+        #v_fea_inv=self.att(v_fea_inv, data_dict['ref_xyz'],data_dict['batch_idx'])
+        #v_fea_inv=self.reshape_2(v_fea_inv)
+
+
+        # fusion and pooling
+        data_dict['sparse_tensor'] = spconv.SparseConvTensor(
+            features=p_fea+v_fea_inv,
+            indices=data_dict['coors'],
+            spatial_shape=self.spatial_shape,
+            batch_size=data_dict['batch_size']
+        )
+        
+
+
+        return  p_fea[coors_inv]##self.reshape_2(self.att(self.reshape_1(p_fea[coors_inv]), data_dict['ref_xyz'],data_dict['batch_idx']))#
+
+
+class get_model(LightningBaseModel):
+    def __init__(self, config):
+        super(get_model, self).__init__(config)
+        self.save_hyperparameters()
+        self.input_dims = config['model_params']['input_dims']
+        self.hiden_size = config['model_params']['hiden_size']
+        self.num_classes = config['model_params']['num_classes']
+        self.scale_list = config['model_params']['scale_list']
+        self.num_scales = len(self.scale_list)
+        min_volume_space = config['dataset_params']['min_volume_space']
+        max_volume_space = config['dataset_params']['max_volume_space']
+        self.coors_range_xyz = [[min_volume_space[0], max_volume_space[0]],
+                                [min_volume_space[1], max_volume_space[1]],
+                                [min_volume_space[2], max_volume_space[2]]]
+        self.spatial_shape = np.array(config['model_params']['spatial_shape'])
+        self.strides = [int(scale / self.scale_list[0]) for scale in self.scale_list]
+
+        # voxelization
+        self.voxelizer = voxelization(
+            coors_range_xyz=self.coors_range_xyz,
+            spatial_shape=self.spatial_shape,
+            scale_list=self.scale_list
+        )
+
+        # input processing
+        self.voxel_3d_generator = voxel_3d_generator(
+            in_channels=self.input_dims,
+            out_channels=self.hiden_size,
+            coors_range_xyz=self.coors_range_xyz,
+            spatial_shape=self.spatial_shape
+        )
+
+        # encoder layers
+        self.spv_enc = nn.ModuleList()
+        for i in range(self.num_scales):
+            self.spv_enc.append(SPVBlock(
+                in_channels=self.hiden_size,
+                out_channels=self.hiden_size,
+                indice_key='spv_'+ str(i),
+                scale=self.scale_list[i],
+                last_scale=self.scale_list[i-1] if i > 0 else 1,
+                spatial_shape=np.int32(self.spatial_shape // self.strides[i])[::-1].tolist())
+            )
+
+        # decoder layer
+        self.classifier = nn.Sequential(
+            nn.Linear(self.hiden_size * self.num_scales//2, 128),
+            nn.ReLU(True),
+            nn.Linear(128, self.num_classes),
+        )
+
+        # loss
+        self.criterion = criterion(config)
+
+        #SF
+        #'''
+        dpr = [x.item() for x in torch.linspace(0, 0.3, 7)]
+        voxel_size=[0.05, 0.05, 0.05]
+        patch_size =np.array([voxel_size[i] * 1 for i in range(3)]).astype(np.float32)
+        window_size = patch_size *6
+        window_size_sphere=np.array([2, 2, 80])
+        sf_c=32
+        self.reshape_1 = nn.Sequential(
+            nn.Linear(self.hiden_size*4 , sf_c),
+            nn.ReLU(True),
+            #nn.Linear(self.hiden_size//2, self.hiden_size//2)
+            )
+        self.att=SphereFormer(
+               sf_c, 
+                (sf_c)//16, 
+                window_size=window_size, 
+                window_size_sphere=window_size_sphere, 
+                quant_size=window_size / 24, 
+                quant_size_sphere=window_size_sphere / 24,
+                indice_key='sphereformer{}'.format(0),
+                rel_query=True, 
+                rel_key=True, 
+                rel_value=True, 
+                drop_path=dpr[0],
+                a=0.0125,
+                )
+        
+        self.reshape_2 = nn.Sequential(
+            nn.Linear(sf_c , self.hiden_size*2),
+            nn.ReLU(True),
+            #nn.Linear(self.hiden_size, self.hiden_size)
+            )#     
+         #'''
+
+    def forward(self, data_dict):
+        with torch.no_grad():
+            data_dict = self.voxelizer(data_dict)
+
+        data_dict = self.voxel_3d_generator(data_dict)
+
+        enc_feats = []
+        for i in range(self.num_scales):
+            enc_feats.append(self.spv_enc[i](data_dict))
+
+        output = torch.cat(enc_feats, dim=1)
+        ##import pdb;pdb.set_trace()
+        output=self.reshape_2(self.att(self.reshape_1(output),data_dict['ref_xyz'],data_dict['batch_idx']))
+        data_dict['logits'] = self.classifier(output)
+
+        data_dict['loss'] = 0.
+        data_dict = self.criterion(data_dict)
+
+        return data_dict
+
+
+class criterion(nn.Module):
+    def __init__(self, config):
+        super(criterion, self).__init__()
+        self.config = config
+        self.lambda_lovasz = self.config['train_params'].get('lambda_lovasz', 0.1)
+        if 'seg_labelweights' in config['dataset_params']:
+            seg_num_per_class = config['dataset_params']['seg_labelweights']
+            seg_labelweights = seg_num_per_class / np.sum(seg_num_per_class)
+            seg_labelweights = torch.Tensor(np.power(np.amax(seg_labelweights) / seg_labelweights, 1 / 3.0))
+        else:
+            seg_labelweights = None
+
+        self.ce_loss = nn.CrossEntropyLoss(
+            weight=seg_labelweights,
+            ignore_index=config['dataset_params']['ignore_label']
+        )
+        self.lovasz_loss = Lovasz_loss(
+            ignore=config['dataset_params']['ignore_label']
+        )
+
+    def forward(self, data_dict):
+        loss_main_ce = self.ce_loss(data_dict['logits'], data_dict['labels'].long())
+        loss_main_lovasz = self.lovasz_loss(F.softmax(data_dict['logits'], dim=1), data_dict['labels'].long())
+        loss_main = loss_main_ce + loss_main_lovasz * self.lambda_lovasz
+        data_dict['loss_main_ce'] = loss_main_ce
+        data_dict['loss_main_lovasz'] = loss_main_lovasz
+        data_dict['loss'] += loss_main
+
+        return data_dict
